5a5e47ac98633ebef776a2c5ba34e89a6b97eb91
Ann Marie Reinhold <ann.marie.reinhold@gmail.com>
2020-03-25 11:30:14 -0600

    Cleaned up error in the initialize boundaries code. Added code to the WQModel to generate the cells and boundaries as part of the model. Minor documentation improvements.

--- R/Class_Boundary.R
+++ R/Class_Boundary.R
@@ -3,8 +3,12 @@
 #' @description Boundary superclass
 #'
 #' @param boundaryIdx the name of the boundary.
+#' @param currency the name of the currency handled by the boundary as a character e.g., \code{H20}
+#' @param boundarySuperClass the super class of the boundary, e.g., \code{transport} or \code{reaction}
 #' @param upstreamCellIdx  the name of the upstream cell as a character.
 #' @param downstreamCellIdx the name of the downstream cell as a character.
+#'
+#' @export
 #'
 Boundary <-
   R6::R6Class(
@@ -16,13 +20,15 @@ Boundary <-
         boundarySuperClass = NULL,
         upstreamCellIdx = NULL,
         downstreamCellIdx = NULL,
+        calculateOrder = NULL,
         initialize =
-          function(){
+          function(boundaryIdx, currency, boundarySuperClass, upstreamCellIdx, downstreamCellIdx, calculateOrder){
             self$boundaryIdx <- boundaryIdx
             self$currency <- currency
             self$boundarySuperClass <- boundarySuperClass
             self$upstreamCellIdx <- upstreamCellIdx
             self$downstreamCellIdx <- downstreamCellIdx
+            self$calculateOrder <- calculateOrder
           }
 
       )
--- R/Class_WQModel.R
+++ R/Class_WQModel.R
@@ -22,13 +22,66 @@ WQModel <-
       list(
         boundsTable = NULL,
         cellsTable = NULL,
+        cells = NULL,
+        bounds = NULL,
+
         initialize =
           function(boundsTable, cellsTable) {
             self$boundsTable <- boundsTable
             self$cellsTable <- cellsTable
-          }
-      )
-  )
+
+            # generate the stream cells from the cellsTable
+            self$cells <-
+              plyr::llply(
+                1:nrow(cellsTable),
+                function(rowNum){
+                  StreamCell$new(
+                    cellIdx = cellsTable$cellIdx[rowNum],
+                    soluteConcentration = cellsTable$initConcNO3[rowNum],
+                    discharge = cellsTable$discharge[rowNum],
+                    alpha = cellsTable$alpha[rowNum],
+                    aquiferVolume = cellsTable$aquiferVolume[rowNum],
+                    porosity = cellsTable$porosity[rowNum],
+                    channelWidth = cellsTable$channelWidth[rowNum],
+                    channelLength = cellsTable$channelLength[rowNum],
+                    channelArea = cellsTable$channelLength[rowNum],
+                    channelDepth = cellsTable$channelDepth[rowNum],
+                    channelVelocity = cellsTable$channelVelocity[rowNum],
+                    channelResidenceTime = cellsTable$channelResidenceTime[rowNum],
+                    qStorage = cellsTable$qStorage[rowNum],
+                    hydraulicLoad = cellsTable$hydraulicLoad[rowNum],
+                    tauMin = cellsTable$tauMin[rowNum],
+                    tauMax = cellsTable$tauMax[rowNum]
+                  )
+                }
+              )
+
+            # Generate the boundaries from the boundsTable in the model
+
+            # First, order the bounds table by the calculate order column. This
+            # ordering makes it so that you can call the boundaries in the the
+            # correct order simply by looping through the list of bounds.
+            boundsTable <- boundsTable[order(boundsTable$calculateOrder), ]
+
+            self$bounds <-
+              plyr::llply(
+              1:nrow(boundsTable),
+              function(rowNum) {
+                Boundary$new(
+                  boundaryIdx = boundsTable$boundaryIdx[rowNum],
+                  currency = boundsTable$currency[rowNum],
+                  boundarySuperClass = boundsTable$boundarySuperClass[rowNum],
+                  upstreamCellIdx = boundsTable$upstreamCellIdx[rowNum],
+                  downstreamCellIdx = boundsTable$downstreamCellIdx[rowNum],
+                  calculateOrder = boundsTable$calculateOrder[rowNum]
+                )
+              }
+            )
+
+
+          } # closes initialize function
+      ) # closes public list
+  ) # closes WQ model
 
 
 
